e966e7f3261d3b1773c53d8f6816d351a462a294
al-shaimon <alshaimon968@gmail.com>
2022-09-23 23:55:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadComments></LoadComments>
+    </div>
+  );
+}
+
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, []);
+
+  return (
+    <div >
+      <h2> Comments: {comments.length}</h2>
+      {comments.map((comment) => (
+        <Comment email={comment.email} body={comment.body}></Comment>
+      ))}
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div style={{
+      backgroundColor: 'lightsalmon',
+      margin: '20px',
+      padding: '20px',
+      border: '5px solid red',
+      borderRadius: '20px',
+    }}>
+      <h4>Email: {props.email}</h4>
+      <p>{props.body}</p>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => setCount(count + 1);
+
+  const handleDecrease = () => setCount(count - 1);
+
+  return (
+    <div
+      style={{
+        backgroundColor: 'goldenrod',
+        margin: '20px',
+        padding: '20px',
+        border: '5px solid red',
+        borderRadius: '20px',
+      }}
+    >
+      <h1>Counter: {count}</h1>
+      <button onClick={handleIncrease} style={{ backgroundColor: 'green', margin: '2px' }}>
+        Increase
+      </button>
+      <button onClick={handleDecrease} style={{ backgroundColor: 'red', margin: '2px' }}>
+        Decrease
+      </button>
     </div>
   );
 }
